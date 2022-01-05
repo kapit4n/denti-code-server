129748fbf41d406108000776acfa72c127d556a4
Luis Arce <luis.arce22@gmail.com>
2022-01-05 17:16:40 -0400

    Appointments: Model and migration file

---
+++ models/appointment.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Appointment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+
+      this.belongsTo(models.Record, {
+        foreignKey: 'recordId',
+        onDelete: 'CASCADE'
+      });
+    }
+  };
+  Appointment.init({
+    recordId: DataTypes.INTEGER,
+    description: DataTypes.STRING,
+    date: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Appointment',
+  });
+  return Appointment;
+};
