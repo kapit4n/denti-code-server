71713cde15af5c5fb9fe71e1d04746af2e71a7bf
Luis Arce <luis.arce22@gmail.com>
2022-01-05 17:41:50 -0400

    Migration: Update migrations

--- migrations/20220101201241-create-record.js
+++ migrations/20220101201241-create-record.js
@@ -20,6 +20,9 @@ module.exports = {
       description: {
         type: Sequelize.STRING
       },
+      price: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
